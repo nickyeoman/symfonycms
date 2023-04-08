490f16f1487a617242cdf3dcee8240ff6d58a760
Nick Yeoman <c@nickyeoman.com>
2023-04-07 22:57:11 -0700

    updated configs

---
+++ src/DependencyInjection/SymfonyCMSExtension.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace NickYeoman\SymfonyCMSBundle\DependencyInjection;
+
+use ReflectionExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class SymfonyCMSExtension extends Extension
+{
+    public fucntion load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            new YamlFileLoader(
+                $container,
+                new FileLocator(paths:__DIR__ . '/../../config'))
+            );
+        );
+
+        $loader->load(resource: 'services.yaml');
+    }
+
+}
--- src/SymfonyCMSBundle.php
+++ src/SymfonyCMSBundle.php
@@ -3,8 +3,18 @@
 namespace NickYeoman\SymfonyCMSBundle;
 
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use NickYeoman\SymfonyCMSBundle\DependencyInjection\SymfonyCMSExtension;
 
 class SymfonyCMSBundle extends AbstractBundle
 {
-    // This is working
+    public function getPath(): string
+    {
+        return dirname(path:__DIR__);
+    }
+
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        return new SymfonyCMSExtension();
+    }
 }
